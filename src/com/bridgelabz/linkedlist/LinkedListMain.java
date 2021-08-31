4d8532b1163fada5966594171e79495743796b6f
varshini <dreamvarshini@gmail.com>
2021-08-31 18:56:33 +0530

    ADDED:created linked list by appending nodes

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -9,11 +9,23 @@ public class LinkedListMain {
 		MyNodeImpl<Integer> mySecondNode= new MyNodeImpl<Integer>(30);
 		MyNodeImpl<Integer> myThirdNode = new MyNodeImpl<Integer>(56);
 		
+		System.out.println("Linked list created by adding");
 		MyLinkedList myLinkedList = new MyLinkedList();
 		myLinkedList.addNode(myFirstNode);
 		myLinkedList.addNode(mySecondNode);
 		myLinkedList.addNode(myThirdNode);
 		myLinkedList.printMyNodes();
+		
+		MyNodeImpl<Integer> firstNode = new MyNodeImpl<Integer>(56);
+		MyNodeImpl<Integer> secondNode= new MyNodeImpl<Integer>(30);
+		MyNodeImpl<Integer> thirdNode = new MyNodeImpl<Integer>(70);
+		
+		System.out.println("Linked list created by appending");
+		MyLinkedList myLinkedListUsingAppend = new MyLinkedList();
+		myLinkedListUsingAppend.appendNode(firstNode);
+		myLinkedListUsingAppend.appendNode(secondNode);
+		myLinkedListUsingAppend.appendNode(thirdNode);
+		myLinkedListUsingAppend.printMyNodes();
 
 	}
 
