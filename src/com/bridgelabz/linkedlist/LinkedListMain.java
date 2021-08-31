5e0c6eaf922db55f535351a08585df99a8327201
varshini <dreamvarshini@gmail.com>
2021-08-31 19:04:53 +0530

    ADDED: created a node and  inserted in between

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -5,27 +5,17 @@ public class LinkedListMain {
 	public static void main(String[] args) 
 	
 	{
-		MyNodeImpl<Integer> myFirstNode = new MyNodeImpl<Integer>(70);
-		MyNodeImpl<Integer> mySecondNode= new MyNodeImpl<Integer>(30);
-		MyNodeImpl<Integer> myThirdNode = new MyNodeImpl<Integer>(56);
 		
-		System.out.println("Linked list created by adding");
-		MyLinkedList myLinkedList = new MyLinkedList();
-		myLinkedList.addNode(myFirstNode);
-		myLinkedList.addNode(mySecondNode);
-		myLinkedList.addNode(myThirdNode);
-		myLinkedList.printMyNodes();
 		
 		MyNodeImpl<Integer> firstNode = new MyNodeImpl<Integer>(56);
 		MyNodeImpl<Integer> secondNode= new MyNodeImpl<Integer>(30);
 		MyNodeImpl<Integer> thirdNode = new MyNodeImpl<Integer>(70);
 		
-		System.out.println("Linked list created by appending");
-		MyLinkedList myLinkedListUsingAppend = new MyLinkedList();
-		myLinkedListUsingAppend.appendNode(firstNode);
-		myLinkedListUsingAppend.appendNode(secondNode);
-		myLinkedListUsingAppend.appendNode(thirdNode);
-		myLinkedListUsingAppend.printMyNodes();
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.addNode(firstNode);
+		myLinkedList.appendNode(thirdNode);
+		myLinkedList.insertNode(firstNode, secondNode);
+		myLinkedList.printMyNodes();
 
 	}
 
